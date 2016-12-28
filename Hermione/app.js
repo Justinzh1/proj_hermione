1fceda7e9738513ac2749e466c008649fcf9e46b
Justin Zhong <Justinzh@Home.local>
2016-12-27 23:54:37 -0800

    Finished routing for form

--- Hermione/app.js
+++ Hermione/app.js
@@ -5,14 +5,31 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
+
+// passport.use(new GoogleStrategy({
+//     consumerKey: GOOGLE_CONSUMER_KEY,
+//     consumerSecret: GOOGLE_CONSUMER_SECRET,
+//     callbackURL: "http://www.example.com/auth/google/callback"
+//   },
+//   function(token, tokenSecret, profile, done) {
+//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//         return done(err, user);
+//       });
+//   }
+// ));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/images", express.static(__dirname + '/images'));
 app.use('/', index);
 app.use('/users', users);
+app.use('/login/', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,5 +61,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
